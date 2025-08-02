79cd89c9e315cd50acbb9bca5db5cc61f445260c
hadar <hadarzim@gmail.com>
2025-08-02 14:42:11 +0300

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by admin on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
